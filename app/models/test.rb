50f6c2c62e53ac54cc7df79b5a17e2f56848a39b
Ksenya <imasylum@yandex.ru>
2020-09-28 20:48:52 +0300

    join-table4

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,9 @@ class Test < ApplicationRecord
   has_many :users
 
   def self.category(category_name)
-    Test.joins('JOIN categories ON tests.category_id = categories.id').where(c_title = category_name).order(title: :desc).pluck(:title)
-		
+    Test
+      .joins('JOIN categories ON tests.category_id = categories.id')
+      .where(c_title = category_name)
+      .order(title: :desc).pluck(:title)	
   end
 end
