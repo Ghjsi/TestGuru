2cb412aa90873b00306ee5d50d6788f6e182fe84
Ksenya <imasylum@yandex.ru>
2020-10-05 00:42:25 +0300

    join-table5

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,7 +4,7 @@ class Test < ApplicationRecord
   def self.category(category_name)
     Test
       .joins('JOIN categories ON tests.category_id = categories.id')
-      .where(c_title = category_name)
+      .where(c_title = category_name) #c_title
       .order(title: :desc).pluck(:title)	
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   has_many :tests
 
   def show_tests(level)
-
-  # id = self.id
   
-  Test.joins('JOIN results ON tests.id = results.test_id').where(level: level, user_id: id)
+  Test
+    .joins('JOIN results ON tests.id = results.test_id')
+    .where(level: level, user_id: users.id)
 
   end
 
